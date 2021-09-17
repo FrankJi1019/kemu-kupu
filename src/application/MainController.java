8872ed2061fa3aebb8766ed3b4a923312f192862
FrankJi3 <fji294@aucklanduni.ac.nz>
2021-09-17 14:04:31 +1200

    Did QuizController

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -20,7 +20,7 @@ public class MainController {
 	}
 	
 	public void newGame(ActionEvent e) throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource("/application/Quiz.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("/application/Main.fxml"));
 		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
