278940cff248ac77c90f5adec8be017daf4938bb
FrankJi3 <fji294@aucklanduni.ac.nz>
2021-09-17 21:12:22 +1200

    initialize quizController
    includes set score, show word count and letter cound, etc

--- src/application/QuizController.java
+++ src/application/QuizController.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
@@ -28,28 +29,58 @@ public class QuizController implements Initializable {
 	private Label speedLabel;
 	@FXML
 	private Slider speedSlider;
+	@FXML
+	private TextField userAnswerTextField;
 	
+	// all words from file
 	private static List<String> allWords;
 	private List<String> words = new ArrayList<String>();
+	
+	private int score = 0;
+	private int wordLetterCount = -1;
+	private List<Word> wordStats = new ArrayList<Word>();
+	
+	private int attemptTimes = 1;
 	
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		// get the five words
 		Random random = new Random();
 		if (allWords.size() <= 5) {
 			words = allWords;
-			System.out.println(this.words);
 			FileIO.speakMaori(this.words.get(1), 1);
-			return;
 		}
 		
 		while (this.words.size() < 5) {
+			if (QuizController.allWords.size() <= 5) break;
 			String word = QuizController.allWords.get(random.nextInt(QuizController.allWords.size()));
 			if (!this.words.contains(word)) {
 				this.words.add(word);
 			}
 		}
-		System.out.println(this.words);
+		
+		// init score the letter count
+		wordLetterCount = this.words.get(0).length();
+		int temp = wordLetterCount;
+		
+		
+		// set word letter count
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < temp; i++) {
+			if (this.words.get(0).charAt(i) == ' ') {
+				sb.append("  ");
+				wordLetterCount--;
+			} else {
+				sb.append("_ ");
+			}
+		}
+		letterCountLabel.setText(String.format("%s(%d letters)", sb.toString(), wordLetterCount));
+		
+		// set which number is being tested
+		wordCountLabel.setText(Integer.toString(6 - this.words.size()));
+		
 	}
 	
 	public static void setWords(List<String> words) {
@@ -61,7 +92,14 @@ public class QuizController implements Initializable {
 	}
 	
 	public void submit() {
-		System.out.println("submit");
+		String userAnswer = userAnswerTextField.getText();
+		if (this.checkWordMatch(userAnswer)) {
+			this.wordStats.add(new Word(this.words.get(0), 1 / this.attemptTimes));
+			this.attemptTimes = 1;
+			this.words.remove(0);
+		} else {
+			
+		}
 	}
 	
 	public void dontKnow() {
@@ -82,5 +120,16 @@ public class QuizController implements Initializable {
 		return userAnswer.equalsIgnoreCase(this.words.get(0));
 	}
 	
+}
+
+class Word {
+	String word;
+	double score;
+
+	public Word(String word, double score) {
+		this.word = word;
+		this.score = score;
+	}
+
 }
 
