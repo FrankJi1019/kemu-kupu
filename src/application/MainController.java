426eae569e39b6e3c2c4a7ffc4e52f06bd5773de
sma148 <sma148@aucklanduni.ac.nz>
2021-09-17 16:21:57 +1200

    feat added confirmation alert window when [quit] method is called in MainController class

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -2,12 +2,16 @@ package application;
 
 
 import java.io.IOException;
+import java.util.Optional;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 public class MainController {
@@ -16,7 +20,19 @@ public class MainController {
 	private Scene scene;
 	
 	public void quit() {
-		System.exit(0);
+		// added confirmation alert window
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Quiting Game");
+		alert.setHeaderText("Are you sure to Quit?");
+		
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.get() == ButtonType.OK) {
+			System.exit(0);
+		}
+		
+		if (result.get() == ButtonType.CANCEL) {
+			alert.close();
+		}
 	}
 	
 	public void newGame(ActionEvent e) throws IOException {
