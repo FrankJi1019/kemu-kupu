f0d4e3dace67c8fbd6c93d64ee25af60704c0a6d
FrankJi3 <fji294@aucklanduni.ac.nz>
2021-10-04 20:03:26 +1300

    add practice button on the main menu

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -53,6 +53,14 @@ public class MainController implements Initializable{
 		}
 	}
 	
+	public void practice(ActionEvent e) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/application/Practice.fxml"));
+		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
 	/** 
 	 * this method switches to Topics scene when called.
 	 */
---
+++ src/application/PracticeController.java
@@ -0,0 +1,18 @@
+package application;
+
+public class PracticeController {
+	
+	public void submit() {
+		System.out.println("SUbmit");
+	}
+	
+	public void hearAgain() {
+		System.out.println("hear again");
+	}
+	
+	public void idk() {
+		System.out.println("skip");
+	}
+	
+
+}
