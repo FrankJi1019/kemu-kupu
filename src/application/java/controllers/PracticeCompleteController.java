8b90bd49df0b3e5cd46f7563281b837ac72fcdb8
sma148 <sma148@aucklanduni.ac.nz>
2021-10-12 18:50:46 +1300

    implement random encouraging method for PracticeCompleteController.java

--- src/application/java/controllers/PracticeCompleteController.java
+++ src/application/java/controllers/PracticeCompleteController.java
@@ -2,6 +2,10 @@ package application.java.controllers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 import javafx.animation.FillTransition;
@@ -26,6 +30,12 @@ public class PracticeCompleteController implements Initializable {
 	
 	private String correctAnswer = "";
 	private String userAnswer = "";
+	
+	private static List<String> SECOND_INCORRECT_MESSAGE = new ArrayList<>(Arrays.asList(
+			"Incorrect, don't worry, learning is a process...",
+			"Incorrect, good luck next time!",
+			"Incorrect, failure is the mother of success!",
+			"Incorrect, don't give up, keep going!"));
 		
 	@FXML private Label answerLabel;
 	@FXML private Rectangle feedbackRect;
@@ -48,7 +58,7 @@ public class PracticeCompleteController implements Initializable {
 			fillTransition.play();
 		} else {
 			FillTransition fillTransition = new FillTransition(Duration.millis(1500),feedbackRect,Color.web("#f87676"), Color.web("#91b2eb"));
-			feedbackLabel.setText("No worries, practice makes perfect!");
+			setEncouragingMessage();
 			fillTransition.play();
 		}
 		
@@ -60,6 +70,16 @@ public class PracticeCompleteController implements Initializable {
 	
 	public void returnHome(ActionEvent e) {
 		this.switchScene("Main", e);
+	}
+	
+	/*
+	 * This method sets random encouraging message when called.
+	 */
+	
+	public void setEncouragingMessage() {
+		Random rand = new Random();
+		// select a random encouraging message from the message list.
+		feedbackLabel.setText(SECOND_INCORRECT_MESSAGE.get((rand.nextInt(SECOND_INCORRECT_MESSAGE.size()))));
 	}
 	
 	/**
